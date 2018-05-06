d1749c7d2ee63500c676287512ff242e5cf6d23c
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-06 15:07:30 +0530

    update login.py

--- login.py
+++ login.py
@@ -27,40 +27,6 @@ def printHTMLend():
     # End html page
     print("</html>")
 
-# def printHTMLbodywithRegisterForm():
-#     # Print body of the html page
-#     print("<body>")
-#     print("<div class=\"container pt-5\">")
-#     print("<h2>Register</h2><br><br>")
-#     # Print signup form
-#     print("<form class=\"card p-5\" action=\"login.py\" method=\"POST\">")
-#     print("<div class=\"form-group\">")
-#     print("<label for=\"exampleInputAccountNum1\">Account Number</label>")
-#     print("<input type=\"text\" class=\"form-control\" id=\"exampleInputAccountNum1\" aria-describedby=\"accountHelp\" placeholder=\"Enter Account no.\" name=\"ac_n\" required>")
-#     print("<small id=\"accountHelp\" class=\"form-text text-muted\">Please enter your bank account number.</small>") 
-#     print("</div>") 
-#     print("<div class=\"form-group\">")
-#     print("<label for=\"exampleInputFullName1\">Full Name</label>")
-#     print("<input type=\"text\" class=\"form-control\" id=\"exampleInputFullName1\" aria-describedby=\"nameHelp\" placeholder=\"Enter your full name associated with account\" name=\"f_nm\" required>")
-#     print("<small id=\"nameHelp\" class=\"form-text text-muted\">Please enter your bank account number.</small>") 
-#     print("</div>") 
-#     print("<div class=\"form-group\">")
-#     print("<label for=\"exampleInputEmail1\">Email address</label>")
-#     print("<input type=\"email\" class=\"form-control\" id=\"exampleInputEmail1\" aria-describedby=\"emailHelp\" placeholder=\"Enter email\" name=\"em_id\" required>") 
-#     print("<small id=\"emailHelp\" class=\"form-text text-muted\">We'll never share your email with anyone else.</small>") 
-#     print("</div>") 
-#     print("<div class=\"form-group\">") 
-#     print("<label for=\"exampleInputPassword1\">Password</label>") 
-#     print("<input type=\"password\" class=\"form-control\" id=\"exampleInputPassword1\" placeholder=\"Password\" name=\"pass\" required>") 
-#     print("</div>")
-#     # print("<div class=\"form-group form-check\">") 
-#     # print("<input type=\"checkbox\" class=\"form-check-input\" id=\"exampleCheck1\">") 
-#     # print("<label class=\"form-check-label\" for=\"exampleCheck1\">Check me out</label>") 
-#     # print("</div>") 
-#     print("<br><button type=\"submit\" class=\"btn btn-primary\">Register now!</button>") 
-#     print("</form>") 
-#     print("</div>") 
-
 def printHTMLbody():
     # Print body of the html page
     print("<body>")
