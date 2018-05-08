0699eecba876be28a36c2ee2c10b12ca68b97a18
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-08 14:25:22 +0530

    remove script

--- deposit_request.py
+++ deposit_request.py
@@ -8,7 +8,6 @@ from mysql.connector import errorcode
 def printHTMLhead():
     # Print head of the html page
     print("<head>")
-    print("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css\" integrity=\"sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB\" crossorigin=\"anonymous\">")
     print("<title>requestpage</title>")
     print("</head>")
 
@@ -19,10 +18,7 @@ def printHTMLstart():
 
 def printHTMLend():
     # Add script tags
-    print("<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>")
     print("<script>window.onunload=function(){window.opener.location.reload();}</script>")
-    print("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js\" integrity=\"sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49\" crossorigin=\"anonymous\"></script>")
-    print("<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js\" integrity=\"sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T\" crossorigin=\"anonymous\"></script>")
     # Print closing body tag for html
     print("</body>")
     # End html page
